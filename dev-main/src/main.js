02882f6a9d1bf5fee52350f9de05bc3586dff44f
Vitalie Rosescu <vitalier@hotmail.be>
2025-02-14 16:52:04 +0100

    please be final

--- dev-main/src/main.js
+++ dev-main/src/main.js
@@ -10,6 +10,7 @@ const TERM_PERCENTAGES = {
   36: 0.16,
   48: 0.16,
 }
+console.log('test')
 
 let term = document.querySelector('input[name="durationInMonths"]:checked').value
 
@@ -158,6 +159,10 @@ class CalculatorApp {
     })
   }
 
+  getDifference(a, b) {
+    return Math.abs(a - b)
+  }
+
   updateUI(result, data) {
     this.calculatorDefault.classList.add('hide')
     this.calculatorResult.style.display = 'block'
@@ -177,15 +182,13 @@ class CalculatorApp {
   updateCalculationResults(r, data, calculatePercentage) {
     console.log(r)
 
-    const beforeEmployerCostTotal =
-      r.beforeLeaseSalary.employerCosts.otherExpenses + r.beforeLeaseSalary.employerCosts.socialSecurityContributions + r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction
-
-    const afterEmployerCostTotal =
-      r.afterLeaseSalary.employerCosts.otherExpenses + r.afterLeaseSalary.employerCosts.socialSecurityContributions + r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction
+    const beforeEmployerCostTotal = r.beforeLeaseSalary.employerCosts.holidayBonus + r.beforeLeaseSalary.employerCosts.sectoralPremium + r.beforeLeaseSalary.employerCosts.yearEndPremium
+    const afterEmployerCostTotal = r.afterLeaseSalary.employerCosts.holidayBonus + r.afterLeaseSalary.employerCosts.sectoralPremium + r.afterLeaseSalary.employerCosts.yearEndPremium
 
     // Calculate monthly lease price and advantage based on financing method
     let monthlyLeasePriceNumber
     let advantagePerMonthNumber
+    let recoupNumber
 
     // Update bonus-related elements visibility
 
@@ -194,32 +197,50 @@ class CalculatorApp {
 
     switch (data.financingMethod) {
       case 'BONUS':
-        monthlyLeasePriceNumber = r.leasePrice.monthlyLeasePrice * VAT_RATE * 12
-        // advantagePerMonthNumber = beforeEmployerCostTotal - afterEmployerCostTotal
-        advantagePerMonthNumber = monthlyLeasePriceNumber + r.diffs.grossSalary
-        document.querySelectorAll('[data-bonus="show"]').forEach((el) => el.classList.remove('hide'))
-        document.querySelectorAll('[data-yearend="hide"],[data-yearend="show"]').forEach((el) => el.classList.add('hide'))
+        monthlyLeasePriceNumber = r.leasePrice.monthlyLeasePrice * VAT_RATE * term
+        advantagePerMonthNumber =
+          this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityContributions, r.afterLeaseSalary.employerCosts.socialSecurityContributions) +
+          this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction, r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction) +
+          this.getDifference(r.beforeLeaseSalary.employerCosts.otherExpenses, r.afterLeaseSalary.employerCosts.otherExpenses)
+        recoupNumber = r.diffs.grossBonus
+        console.log(recoupNumber)
+        document.querySelectorAll('[data-bonus="show"').forEach((el) => el.classList.remove('hide'))
+        document.querySelectorAll('[data-yearend="hide"],[data-yearend="show"],[data-after]').forEach((el) => el.classList.add('hide'))
+        document.querySelectorAll('.calculator_output-item').forEach((el) => el.classList.add('is-bonus'))
+        document.querySelectorAll('[data-bonus-alt]').forEach((el) => el.classList.add('is--alternate'))
+        document.querySelectorAll('[data-bonus-regular]').forEach((el) => el.classList.remove('is--alternate'))
         break
       case 'YEAR_END_PREMIUM':
-        monthlyLeasePriceNumber = r.leasePrice.monthlyLeasePrice * VAT_RATE * term
-        // advantagePerMonthNumber = beforeEmployerCostTotal - afterEmployerCostTotal
-        advantagePerMonthNumber = monthlyLeasePriceNumber + r.diffs.grossSalary
+        monthlyLeasePriceNumber = r.leasePrice.monthlyLeasePrice * VAT_RATE * 12
+        advantagePerMonthNumber =
+          this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityContributions, r.afterLeaseSalary.employerCosts.socialSecurityContributions) +
+          this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction, r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction) +
+          this.getDifference(r.beforeLeaseSalary.employerCosts.otherExpenses, r.afterLeaseSalary.employerCosts.otherExpenses)
+        recoupNumber = r.diffs.grossYearEndPremium
         document.querySelectorAll('[data-yearend="show"]').forEach((el) => el.classList.remove('hide'))
-        document.querySelectorAll('[data-bonus="hide"], [data-bonus="show"]').forEach((el) => el.classList.add('hide'))
+        document.querySelectorAll('[data-bonus="hide"], [data-bonus="show"], [data-after]').forEach((el) => el.classList.add('hide'))
+
+        document.querySelectorAll('.calculator_output-item').forEach((el) => el.classList.add('is-bonus'))
+        document.querySelectorAll('[data-bonus-alt]').forEach((el) => el.classList.add('is--alternate'))
+        document.querySelectorAll('[data-bonus-regular]').forEach((el) => el.classList.remove('is--alternate'))
         break
       default:
         monthlyLeasePriceNumber = r.leasePrice.monthlyLeasePrice * VAT_RATE
-        // advantagePerMonthNumber = (beforeEmployerCostTotal - afterEmployerCostTotal) / 12
         advantagePerMonthNumber = monthlyLeasePriceNumber + r.diffs.grossSalary
+        recoupNumber = r.diffs.grossSalary
         document.querySelectorAll('[data-yearend="show"], [data-bonus="show"]').forEach((el) => el.classList.add('hide'))
-        document.querySelectorAll('[data-yearend="hide"], [data-bonus="hide"]').forEach((el) => el.classList.remove('hide'))
+        document.querySelectorAll('[data-yearend="hide"], [data-bonus="hide"], [data-after]').forEach((el) => el.classList.remove('hide'))
+
+        document.querySelectorAll('.calculator_output-item').forEach((el) => el.classList.remove('is-bonus'))
+        document.querySelectorAll('[data-bonus-alt]').forEach((el) => el.classList.remove('is--alternate'))
+        document.querySelectorAll('[data-bonus-regular]').forEach((el) => el.classList.add('is--alternate'))
     }
 
     // Update calculated values
-    this.updateCalculatedValues(r, data, beforeEmployerCostTotal, afterEmployerCostTotal, monthlyLeasePriceNumber, advantagePerMonthNumber, calculatePercentage)
+    this.updateCalculatedValues(r, data, beforeEmployerCostTotal, afterEmployerCostTotal, monthlyLeasePriceNumber, advantagePerMonthNumber, calculatePercentage, recoupNumber)
   }
 
-  updateCalculatedValues(r, data, beforeEmployerCostTotal, afterEmployerCostTotal, monthlyLeasePriceNumber, advantagePerMonthNumber, calculatePercentage) {
+  updateCalculatedValues(r, data, beforeEmployerCostTotal, afterEmployerCostTotal, monthlyLeasePriceNumber, advantagePerMonthNumber, calculatePercentage, recoupNumber) {
     // Monthly lease price and maintenance
     document.querySelectorAll('[data-result="monthlyLeasePrice"]').forEach((el) => (el.innerText = monthlyLeasePriceNumber.toFixed(1)))
 
@@ -229,21 +250,29 @@ class CalculatorApp {
     this.setValue('servicePackage', document.querySelector('input[name="yearlyMaintenanceFee"]:checked').getAttribute('data-label'), false)
     this.setValue('durationInMonths', document.querySelector('input[name="durationInMonths"]:checked').getAttribute('data-label').split(' ')[0], false, true)
 
-    // Social security calculations
-    this.setValue('beforeSocialSecurity', this.formatNegativeAmount(r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction), false)
-    this.setValue('afterSocialSecurity', this.formatNegativeAmount(r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction), false)
+    // Social security contributions
+    this.setValue('beforeSocialSecurityContribution', r.beforeLeaseSalary.employerCosts.socialSecurityContributions.toFixed(1))
+    this.setValue('afterSocialSecurityContribution', r.afterLeaseSalary.employerCosts.socialSecurityContributions.toFixed(1))
+    this.setValue('beforeSocialSecurityStructuralReduction', this.formatNegativeAmount(r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction), false)
+    this.setValue('afterSocialSecurityStructuralReduction', this.formatNegativeAmount(r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction), false)
 
     // Other expenses
-    this.setValue('beforeOtherExpenses', this.formatNegativeAmount(r.beforeLeaseSalary.employerCosts.otherExpenses), false)
-    this.setValue('afterOtherExpenses', this.formatNegativeAmount(r.afterLeaseSalary.employerCosts.otherExpenses), false)
+    this.setValue('beforeOtherExpenses', r.beforeLeaseSalary.employerCosts.otherExpenses.toFixed(1))
+    this.setValue('afterOtherExpenses', r.afterLeaseSalary.employerCosts.otherExpenses.toFixed(1))
+
+    // Werkgever kost
 
     // Advantages and totals
-    this.setValue('advantage', advantagePerMonthNumber.toFixed(1), true, true)
-    // this.setValue('recoup', (monthlyLeasePriceNumber - advantagePerMonthNumber).toFixed(1), true)
-    this.setValue('recoup', Math.abs(r.diffs.grossSalary).toFixed(1), true, true)
+    this.setValue('advantage', advantagePerMonthNumber.toFixed(1), false, true)
+    this.setValue('recoup', Math.abs(recoupNumber).toFixed(1), false, true)
     this.setValue('beforeEmployerTotal', this.formatNegativeAmount(beforeEmployerCostTotal), false)
     this.setValue('afterEmployerTotal', this.formatNegativeAmount(afterEmployerCostTotal), false)
-    this.setValue('savedTotalYearly', `${(advantagePerMonthNumber * 12).toFixed(1)}`)
+    const savedTotalYearlyNumber =
+      this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityContributions, r.afterLeaseSalary.employerCosts.socialSecurityContributions) +
+      this.getDifference(r.beforeLeaseSalary.employerCosts.socialSecurityStructuralReduction, r.afterLeaseSalary.employerCosts.socialSecurityStructuralReduction) +
+      this.getDifference(r.beforeLeaseSalary.employerCosts.otherExpenses, r.afterLeaseSalary.employerCosts.otherExpenses) +
+      this.getDifference(beforeEmployerCostTotal, afterEmployerCostTotal)
+    this.setValue('savedTotalYearly', savedTotalYearlyNumber.toFixed(1))
 
     // Salary calculations
     this.setValue('beforeLeaseSalaryGross', r.beforeLeaseSalary.grossSalary.toFixed(1))
@@ -252,6 +281,8 @@ class CalculatorApp {
     this.setValue('beforeSpecialSocialSecurityContributions', this.formatNegativeAmount(r.beforeLeaseSalary.specialSocialSecurityContributions), false)
     this.setValue('afterSocialSecurityContributions', this.formatNegativeAmount(r.afterLeaseSalary.socialSecurityContributions), false)
     this.setValue('afterSpecialSocialSecurityContributions', this.formatNegativeAmount(r.afterLeaseSalary.specialSocialSecurityContributions), false)
+    this.setValue('beforeNetSalary', r.beforeLeaseSalary.monthlyNetSalary.toFixed(1))
+    this.setValue('afterNetSalary', r.afterLeaseSalary.monthlyNetSalary.toFixed(1))
 
     // Work bonus and income
     this.setValue('beforeWorkBonus', r.beforeLeaseSalary.workBonus.toFixed(1))
@@ -262,7 +293,7 @@ class CalculatorApp {
     this.setValue('afterIncomeTax', this.formatNegativeAmount(r.afterLeaseSalary.incomeTaxes), false)
 
     // Net calculations
-    this.setValue('monthlyNet', Math.abs(r.diffs.monthlyNet.toFixed(2)))
+    this.setValue('monthlyNet', Math.abs(r.diffs.monthlyNet.toFixed(1)))
     this.setValue('totalNet', Math.abs(r.diffs.totalNet.toFixed(1)))
     this.setValue('residualValue', (data.computeLeaseRateDto.bikeRetailPrice * calculatePercentage * VAT_RATE).toFixed(1))
 
@@ -280,7 +311,7 @@ class CalculatorApp {
   }
 
   formatNegativeAmount(amount) {
-    return amount === 0 ? '€0' : `-€${Math.abs(amount.toFixed(1))}`
+    return amount === 0 ? '€0' : `- €${Math.abs(amount.toFixed(1))}`
   }
 
   async handleSubmit(e) {
